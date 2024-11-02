66eb460dbc3b6a0506a9acc8f9292a07d76ecad2
quanbao366 <quanbao366@gmail.com>
2024-11-02 09:33:23 +0700

    add model

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using FlashShop.Models;
+
+namespace FlashShop.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int Publication { get; set; }
+        public float Point { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FlashShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
