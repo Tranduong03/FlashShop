21233440c3824fe974db5ce4f7961b0eec347654
quanbao366 <quanbao366@gmail.com>
2024-11-04 19:29:35 +0700

    add map

--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Home}/{id?}");
+    pattern: "{controller=Home}/{action=Map}/{id?}");
 
 app.Run();
