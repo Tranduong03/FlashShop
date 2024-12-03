5f0ebe7e826e48279a7d50d2aab401c6bbb41546
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2024-12-03 16:08:22 +0700

    done

--- Areas/Admin/Controllers/BookController.cs
+++ Areas/Admin/Controllers/BookController.cs
@@ -20,26 +20,33 @@ namespace FlashShop.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index(int pg = 1)
         {
-           List<BookModel> book = _dataContext.Books.ToList();
+            const int pageSize = 6;
 
-            const int pageSize = 3;
-
-            if (pg < 1)
+            if (pg < 1) 
             {
                 pg = 1;
             }
-            int ResCount = book.Count;
 
-            var pager = new Paginate(ResCount, pg, pageSize);
+            var booksQuery = _dataContext.Books
+                .Include(b => b.Categories)
+                .Include(b => b.Publisher)
+                .OrderBy(b => b.BookId);
+
+            int resCount = await booksQuery.CountAsync();
+            var pager = new Paginate(resCount, pg, pageSize);
 
             int recSkip = (pg - 1) * pageSize;
 
-            var data = book.Skip(recSkip).Take(pager.PageSize).ToList();
+            var data = await booksQuery
+                .Skip(recSkip)
+                .Take(pager.PageSize)
+                .ToListAsync();
 
             ViewBag.Pager = pager;
 
             return View(data);
         }
+
 
         public IActionResult Create()
         {
@@ -85,12 +92,12 @@ namespace FlashShop.Areas.Admin.Controllers
                 }
                 _dataContext.Add(book);
                 await _dataContext.SaveChangesAsync();
-                TempData["successAdmin"] = "Thêm sách thành công vào Cơ sở dữ liệu";
+                TempData["successAdmin"] = "Thêm sách mới thành công";
                 return RedirectToAction("Index");               
             }
             else
             {
-                TempData["errorAdmin"] = "Model có một vài thứ đang bị lỗi";
+                TempData["errorAdmin"] = "Thêm sách mới thất bại, thử lại !";
                 List<string> errors = new List<string>();
                 foreach (var value in ModelState.Values)
                 {
@@ -126,6 +133,68 @@ namespace FlashShop.Areas.Admin.Controllers
         }
 
 
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, BookModel book)
+        //{
+        //    ViewBag.Categories = new SelectList(_dataContext.Categories, "CategoryId", "CategoryName", book.CategoryId);
+        //    ViewBag.Publishers = new SelectList(_dataContext.Publishers, "PublisherId", "PublisherName", book.PublisherId);
+
+        //    var currentYear = DateTime.Now.Year;
+        //    var years = Enumerable.Range(currentYear - 99, 100).Select(x => x.ToString()).ToList();
+        //    ViewBag.Years = new SelectList(years, book.Publication?.ToString());
+
+        //    var existingBook = await _dataContext.Books.FindAsync(id);
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (existingBook == null)
+        //        {
+        //            TempData["errorAdmin"] = "Không tìm thấy sách với ID được cung cấp!";
+        //            return NotFound();
+        //        }
+
+        //        //var title = await _dataContext.Books.FirstOrDefaultAsync(b => b.Title == book.Title && b.BookId != id);
+        //        //if (title != null)
+        //        //{
+        //        //    ModelState.AddModelError("", "Sản phẩm này đã tồn tại");
+        //        //    TempData["errorAdmin"] = "Tên sản phẩm trùng với sản phẩm khác";
+        //        //    return View(book);
+        //        //}                        
+
+        //        if (book.ImgLinkUpload != null)
+        //        {
+        //            string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "images/book");
+        //            string imageName = Guid.NewGuid().ToString() + "_" + book.ImgLinkUpload.FileName;
+        //            string filePath = Path.Combine(uploadsDir, imageName);
+
+        //            using (var fs = new FileStream(filePath, FileMode.Create))
+        //            {
+        //                await book.ImgLinkUpload.CopyToAsync(fs);
+        //            }
+
+        //            existingBook.ImgLink = imageName;
+        //        }
+
+        //        existingBook.Title = book.Title;
+        //        existingBook.Price = book.Price;
+        //        existingBook.Quantity = book.Quantity;
+        //        existingBook.Description = book.Description;
+        //        existingBook.Author = book.Author;
+        //        existingBook.Publication = book.Publication;
+        //        existingBook.Point = book.Point;
+        //        existingBook.CategoryId = book.CategoryId;
+        //        existingBook.PublisherId = book.PublisherId;
+
+        //        await _dataContext.SaveChangesAsync();
+        //        TempData["successAdmin"] = "Cập nhật thông tin sách thành công";
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    TempData["errorAdmin"] = "Cập nhật thông tin sách không thành công";
+        //    return View(book);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, BookModel book)
@@ -154,10 +223,21 @@ namespace FlashShop.Areas.Admin.Controllers
                     ModelState.AddModelError("", "Sản phẩm này đã tồn tại");
                     TempData["errorAdmin"] = "Tên sản phẩm trùng với sản phẩm khác";
                     return View(book);
-                }                        
+                }
 
                 if (book.ImgLinkUpload != null)
                 {
+                    // Xóa ảnh cũ nếu tồn tại
+                    if (!string.IsNullOrEmpty(existingBook.ImgLink))
+                    {
+                        string oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images/book", existingBook.ImgLink);
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    // Lưu ảnh mới
                     string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "images/book");
                     string imageName = Guid.NewGuid().ToString() + "_" + book.ImgLinkUpload.FileName;
                     string filePath = Path.Combine(uploadsDir, imageName);
@@ -170,6 +250,7 @@ namespace FlashShop.Areas.Admin.Controllers
                     existingBook.ImgLink = imageName;
                 }
 
+                // Cập nhật thông tin sách
                 existingBook.Title = book.Title;
                 existingBook.Price = book.Price;
                 existingBook.Quantity = book.Quantity;
@@ -198,7 +279,6 @@ namespace FlashShop.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            // Xóa ảnh nếu không phải "No_image.jpg"
             if (!string.Equals(book.ImgLink, "No_image.jpg", StringComparison.OrdinalIgnoreCase))
             {
                 string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "images/book");
